fbe5f4b745428b41403fc63b5758ceba087b8aab
Daniel Allott <36487931+Allott@users.noreply.github.com>
2023-04-05 14:00:40 +0100

    working spinning box

--- static-three-app/src/components/Canvas/index.js
+++ static-three-app/src/components/Canvas/index.js
@@ -1,4 +1,23 @@
-import { Canvas } from "@react-three/fiber";
+import { useRef } from 'react'
+import { Canvas, useFrame } from "@react-three/fiber";
+
+
+const Box = (props) => {   
+    // Subscribe this component to the render-loop, rotate the mesh every frame
+    const ref = useRef()
+    useFrame((state, delta) => (ref.current.rotation.x += delta))
+    // Return the view, these are regular Threejs elements expressed in JSX
+    return (
+      <mesh
+        {...props}
+        ref={ref}
+        scale={5}
+      >
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color={'orange'} />
+      </mesh>
+    )
+  }
 
 const canvas = () => {
     return (
@@ -9,7 +28,9 @@ const canvas = () => {
                 position: [-6, 7, 7],
                 }}
             >
-                
+                <ambientLight intensity={0.5} />
+                <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+                <Box position={[-1.2, 0, 0]} />
             </Canvas>
         </div>
    

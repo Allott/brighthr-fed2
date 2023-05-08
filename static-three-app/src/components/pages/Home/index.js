3f2f2c9066a3c99233c2ce77a4c9598c25aa2c6a
Daniel Allott <36487931+Allott@users.noreply.github.com>
2023-05-08 15:29:07 +0100

    all pages

--- static-three-app/src/components/pages/Home/index.js
+++ static-three-app/src/components/pages/Home/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import useScroll from '@/utils/useScroll' 
 import BackgroundCanvas from "@/components/three/BackgroundCanvas";
 import { ScrollBox, FaceBox } from "@/components/ScrollBox";
+import { H2 } from '../../Text'
+import goToPage from '@/utils/goToPage'
 import Landing from '../faces/Landing';
 import Buzz from '../faces/Buzz';
 import Education from '../faces/Education';
 import Career from '../faces/Career';
+import ContactMe from '../faces/ContactMe';
 
 const Home = () => {    
     const scroll = useScroll();
@@ -18,8 +21,12 @@ const Home = () => {
                 <FaceBox position={1}><Buzz /></FaceBox>
                 <FaceBox position={2}><Education /></FaceBox>
                 <FaceBox position={3}><Career /></FaceBox>
-                <FaceBox position={4}>test</FaceBox>
-                <FaceBox position={5}>test</FaceBox>
+                <FaceBox position={4}><ContactMe /></FaceBox>
+                <FaceBox position={5}>
+                    <button onClick={()=> goToPage(0)} className='z-50 mt-20'>
+                        <H2 underline>Back to start?</H2>
+                    </button>
+                </FaceBox>
             </ScrollBox>
         </>
     )
--- static-three-app/src/components/pages/faces/ContactMe/index.js
+++ static-three-app/src/components/pages/faces/ContactMe/index.js
@@ -1,17 +1,17 @@
-import { H1, H2 } from '../../../Text'
+import { H1 } from '../../../Text'
 
 const Landing = () => (
     <div className='flex flex-col items-center'>
-        <H1>Contact Me</H1>
-        <H2 underline>07419117333</H2>
-        <a href='mailto:daniel@llott.co.uk' target="_blank" type='link' className='z-50'>
-            <H2 underline>daniel@llott.co.uk</H2>
+        <H1 className='mb-10'>Contact Me</H1>
+        <H1 className='mt-4'>07419­-117­-333</H1>
+        <a href='mailto:daniel@llott.co.uk' target="_blank" type='link' className='z-50 mt-4'>
+            <H1 underline>daniel@llott.co.uk</H1>
         </a>
-        <a href='https://github.com/Allott' target="_blank" type='link' className='z-50'>
-            <H2 underline>Github</H2>
+        <a href='https://github.com/Allott' target="_blank" type='link' className='z-50 mt-4'>
+            <H1 underline>Github</H1>
         </a>
-        <a href='https://www.linkedin.com/in/daniel-allott-477783184/' target="_blank" type='link' className='z-50'>
-            <H2 underline>Linkedin</H2>
+        <a href='https://www.linkedin.com/in/daniel-allott-477783184/' target="_blank" type='link' className='z-50 mt-4'>
+            <H1 underline>Linkedin</H1>
         </a>
     </div>
 )

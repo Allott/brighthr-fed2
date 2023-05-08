e2c731eba6dd42047c8959676397a489dc5ff426
Daniel Allott <36487931+Allott@users.noreply.github.com>
2023-05-08 12:07:24 +0100

    education page

--- static-three-app/src/components/pages/Home/index.js
+++ static-three-app/src/components/pages/Home/index.js
@@ -4,6 +4,7 @@ import BackgroundCanvas from "@/components/three/BackgroundCanvas";
 import { ScrollBox, FaceBox } from "@/components/ScrollBox";
 import Landing from '../faces/Landing';
 import Buzz from '../faces/Buzz';
+import Education from '../faces/Education';
 
 const Home = () => {    
     const scroll = useScroll();
@@ -14,7 +15,7 @@ const Home = () => {
             <ScrollBox count={7}>
                 <FaceBox position={0}><Landing contactPage={2} /></FaceBox>
                 <FaceBox position={1}><Buzz /></FaceBox>
-                <FaceBox position={2}>2</FaceBox>
+                <FaceBox position={2}><Education /></FaceBox>
                 <FaceBox position={3}>test</FaceBox>
                 <FaceBox position={4}>test</FaceBox>
                 <FaceBox position={5}>test</FaceBox>
---
+++ static-three-app/src/components/pages/faces/Education/index.js
@@ -0,0 +1,31 @@
+import Image from 'next/image'
+import Hull from '/public/pictures/Hull.jpeg'
+import Nottingham from '/public/pictures/Nottingham.jpeg'
+import { H1, H2 } from '../../../Text'
+import goToPage from '@/utils/goToPage'
+
+const Education = () => (
+    <div className='flex flex-col items-center'>
+        <div className='flex flex-row items-center w-full'>
+            <Image src={Hull} alt="Hull" width="175" height="175" className='mr-8 rounded'/>
+            <div>
+                <H1>University of Hull</H1>
+                <H1>BSC computer science</H1>
+                <H2>2015-2018</H2>
+            </div>
+        </div>
+        <div className='flex flex-row items-center w-full my-4'>
+            <div>
+                <H1>University of Nottingham</H1>
+                <H1>MSC computer science</H1>
+                <H2>2018-2019</H2>
+            </div>
+            <Image src={Nottingham} alt="Nottingham" width="175" height="175" className='ml-8 rounded'/>
+        </div>
+        <a href='/docs/Resume.pdf' target="_blank" type='PDF' className='z-50'>
+            <H2 underline>Full Resume</H2>
+        </a>
+    </div>
+)
+
+export default Education;
